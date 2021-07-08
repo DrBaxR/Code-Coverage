20308533cb1f62111c506da6ba5abcf83872591f
Alexandru Florea <alexflorea16000@gmail.com>
2021-07-08 14:27:55 +0300

    Added sample test

---
+++ src/main/kotlin/me/drbaxr/codecoverage/Sample.kt
@@ -0,0 +1,11 @@
+package me.drbaxr.codecoverage
+
+class Sample {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun diff(a: Int, b: Int): Int {
+        return a - b
+    }
+}
