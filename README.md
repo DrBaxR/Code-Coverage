6970c59108c7fc7328d986f6cbe08423646ea7c6
Alexandru Florea <alexflorea16000@gmail.com>
2021-08-14 15:33:47 +0300

    Added tests for java tested unit extractor + added test resources in the git commit

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ```java -jar target/code-coverage-1.0-SNAPSHOT-jar-with-dependencies.jar [args]```
 
 **This only works if there are no compiling errors**
+
+also contains various random repos i found on github (they can be found in the test resources). i don't own any of those
---
+++ src/test/kotlin/me/drbaxr/codecoverage/extractors/testedunit/JavaTestedUnitExtractorTest.kt
@@ -0,0 +1,57 @@
+package me.drbaxr.codecoverage.extractors.testedunit
+
+import me.drbaxr.codecoverage.extractors.testfile.ManualTestFileExtractor
+import me.drbaxr.codecoverage.extractors.unit.java.JavaUnitExtractor
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JavaTestedUnitExtractorTest {
+
+    @Test
+    fun `test demo package`() {
+        val expected = setOf("de.syngenio.demo.MyTestClass", "de.syngenio.demo.MyTestClass.doSomething")
+
+        val ue = JavaUnitExtractor(
+            "src/test/resources/sample-projects/junit-tests-master",
+            ManualTestFileExtractor("src/test/resources/sample-projects/junit-tests-master")
+        )
+
+        val testFile =
+            "src/test/resources/sample-projects/junit-tests-master/src/test/java/de/syngenio/demo/TestMyTestClass.java"
+        val actual = JavaTestedUnitExtractor(ue.findUnits())
+            .findTestedUnits(testFile)
+            .map { it.identifier }.toSet()
+
+        assertEquals(expected, actual)
+
+    }
+
+    @Test
+    fun `test demo2 package`() {
+        val expected = setOf(
+            "de.syngenio.demo3.Actor",
+            "de.syngenio.demo3.Actor.moveMotor",
+            "de.syngenio.demo3.Actor.stopMotor",
+            "de.syngenio.demo3.Controller",
+            "de.syngenio.demo3.Controller.singleDecision",
+            "de.syngenio.demo3.Sensor",
+            "de.syngenio.demo3.Sensor.getBrightness",
+            "de.syngenio.demo3.Sensor.getTemperature",
+            "de.syngenio.demo3.Sensor.isMotorBlocked"
+        )
+
+        val ue = JavaUnitExtractor(
+            "src/test/resources/sample-projects/junit-tests-master",
+            ManualTestFileExtractor("src/test/resources/sample-projects/junit-tests-master")
+        )
+
+        val testFile =
+            "src/test/resources/sample-projects/junit-tests-master/src/test/java/de/syngenio/demo3/TestController.java"
+        val actual = JavaTestedUnitExtractor(ue.findUnits())
+            .findTestedUnits(testFile)
+            .map { it.identifier }.toSet()
+
+        assertEquals(expected, actual)
+    }
+
+}
