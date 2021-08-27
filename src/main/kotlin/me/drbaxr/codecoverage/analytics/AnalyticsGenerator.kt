7bc8802de68239bd6b130b9531659ef098661d9c
Alexandru Florea <alexflorea16000@gmail.com>
2021-08-27 13:35:05 +0300

    Added line coverage statistic and sample of how to use html generator

--- src/main/kotlin/me/drbaxr/codecoverage/analytics/AnalyticsGenerator.kt
+++ src/main/kotlin/me/drbaxr/codecoverage/analytics/AnalyticsGenerator.kt
@@ -7,15 +7,39 @@ class AnalyticsGenerator {
     fun generate(allUnits: List<CodeUnit>, testedUnits: List<CodeUnit>) {
         // TODO: check https://j2html.com/examples.html and make html
 
-        println("All Units Found:")
-        allUnits.forEach { println(it.identifier) }
+//        println("All Units Found:")
+//        allUnits.forEach { println(it.identifier) }
+//
+//        println("\nTested Units Found:")
+//        testedUnits.forEach { println(it.identifier) }
 
-        println("\nTested Units Found:")
-        testedUnits.forEach { println(it.identifier) }
+        println("Code Coverage: ${testedUnits.size.toFloat() / allUnits.size.toFloat() * 100}%")
+        println("Line Coverage: ${calculateLineCoverage(allUnits, testedUnits)}%")
 
-        println("\nCode Coverage For Units: ${testedUnits.size.toFloat() / allUnits.size.toFloat() * 100}%")
+        HtmlGenerator().generate()
+    }
 
-        // TODO: also add line coverage
+    private fun calculateLineCoverage(allUnits: List<CodeUnit>, testedUnits: List<CodeUnit>): Float {
+        val totalTestableLines = allUnits.fold(0.0f) { acc, unit -> acc + unit.linesRange.last - unit.linesRange.first }
+        val testedLines = testedUnits.fold(0.0f) { acc, unit -> acc + unit.linesRange.last - unit.linesRange.first }
+
+        return testedLines / totalTestableLines * 100
+    }
+
+    private fun getFilesToUnitsMap(units: List<CodeUnit>): Map<String, Set<CodeUnit>> {
+        val filesToUnitsMap = mutableMapOf<String, MutableSet<CodeUnit>>()
+
+        units.forEach {
+            val unitsSet = filesToUnitsMap[it.hostFilePath]
+
+            if (unitsSet != null) {
+                unitsSet.add(it)
+            } else {
+                filesToUnitsMap[it.hostFilePath] = mutableSetOf()
+            }
+        }
+
+        return filesToUnitsMap
     }
 
 }
